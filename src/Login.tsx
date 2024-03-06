5fc446d9716758ad96b45a0f3177847a83d95418
Despo <tornike.despotashvili@gmail.com>
2024-03-07 00:53:55 +0400

    create auth svgs and import button components to login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import EyeIcon from './components/eyeIcon/EyeIcon'
+import LoginButtonComp from './components/loginButtons/LoginButtonComp'
+import EmailSVG from './components/SVG/EmailSVG'
+import FacebookSVG from './components/SVG/facebookSVG'
+import GoogleSVG from './components/SVG/googleSVG'
+
 
 const Login = () => {
   return (
     <div className='login'>
+        
+        <EyeIcon />
+
         <div className='buttons'>
-            
+            <LoginButtonComp icon={<GoogleSVG/>} text='Log in with Google' />
+            <LoginButtonComp icon={<FacebookSVG/>} text='Log in with Facebook' />
+            <LoginButtonComp icon={<EmailSVG/>} text='Log in with Email' />
         </div>
     </div>
   )
--- src/components/loginButtons/LoginButtonComp.tsx
+++ src/components/loginButtons/LoginButtonComp.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import "./LoginButtonComp.css"
+import React, { ReactNode } from "react";
+import "./LoginButtonComp.css";
 
 interface LoginButtonProps {
-    icon: string;
-    text: string;
+  icon: ReactNode;
+  text: string;
 }
 
-const LoginButtonComp: React.FC<LoginButtonProps> = ({icon, text}) => {
+const LoginButtonComp: React.FC<LoginButtonProps> = ({ icon, text }) => {
   return (
-    <div className='login_button'>
-        <img src={icon} alt='login icon' style={{width:"30px", height:"30px"}} />
-        <p>{text}</p>
+    <div className="login_button">
+      {icon}
+      <p>{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default LoginButtonComp
+export default LoginButtonComp;
