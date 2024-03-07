ee72836c19afcf5dca44fd092671244e69901643
Despo <tornike.despotashvili@gmail.com>
2024-03-08 00:54:41 +0400

    create timer

--- src/components/GameQuestion/Timer.tsx
+++ src/components/GameQuestion/Timer.tsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./GameQuestion.css"
 
 export const Timer = () => {
 
     const [seconds, setSeconds] = useState(30)
 
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setSeconds(seconds - 1)
+        },1000)
+
+        if(seconds <= 0){
+            clearInterval(timer)
+        }
+
+        return () => clearInterval(timer)
+    },[seconds])
+
   return (
     <div className='Timer'>
         <div className='timer_time'>
-            <p>00 : {seconds}</p>
+            <p>00 : {seconds < 10 ? `0${seconds}` : seconds }</p>
         </div>
         <div className='timer_bar'>
-            <div className='timer_progress' style={{width:'70%'}}/>
+            <div className='timer_progress'/>
         </div>
     </div>
   )
