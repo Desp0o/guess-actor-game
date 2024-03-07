257713919cc207e67edf2abb9b1737c72006ca16
Despo <tornike.despotashvili@gmail.com>
2024-03-07 15:51:49 +0400

    implement google Auth

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,25 +1,84 @@
-import EmailSVG from './components/SVG/EmailSVG'
-import FacebookSVG from './components/SVG/FacebookSVG'
-import GoogleSVG from './components/SVG/GoogleSVG'
-import EyeIcon from './components/eyeIcon/EyeIcon'
-import LoginButtonComp from './components/loginButtons/LoginButtonComp'
-
+import { useLayoutEffect, useState } from "react";
+import EmailSVG from "./components/SVG/EmailSVG";
+import FacebookSVG from "./components/SVG/FacebookSVG";
+import GoogleSVG from "./components/SVG/GoogleSVG";
+import EyeIcon from "./components/eyeIcon/EyeIcon";
+import LoginButtonComp from "./components/loginButtons/LoginButtonComp";
+import {
+  GoogleAuthProvider,
+  browserLocalPersistence,
+  getAuth,
+  onAuthStateChanged,
+  setPersistence,
+  signInWithPopup,
+} from "firebase/auth";
+import { app } from "./components/FirebaseConfig";
+import { useDispatch } from "react-redux";
+import { setUser } from "./store/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [isLoading, setLoading] = useState(false);
+
+  const googleLogIn = async () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    try {
+      await setPersistence(auth, browserLocalPersistence);
+      const signIn = await signInWithPopup(auth, provider);
+      console.log(signIn);
+      dispatch(setUser(signIn.user.displayName));
+      return signIn;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    setLoading(true)
+
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.displayName));
+        setLoading(false);
+        navigate("/pages/home");
+      }else {
+        setLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch, navigate]);
+
+  if (isLoading) {
+    return <div className="">loading</div>;
+  }
+
   return (
-    <div className='login'>
-        
-        <EyeIcon />
+    <div className="login">
+      <EyeIcon />
 
-        <div className='login_page_buttons'>
-            <LoginButtonComp icon={<GoogleSVG/>} text='Log in with Google' />
-            <LoginButtonComp icon={<FacebookSVG/>} text='Log in with Facebook' />
-            <LoginButtonComp icon={<EmailSVG/>} text='Log in with Email' />
-        </div>
+      <div className="login_page_buttons">
+        <LoginButtonComp
+          icon={<GoogleSVG />}
+          text="Log in with Google"
+          funName={googleLogIn}
+        />
+        <LoginButtonComp icon={<FacebookSVG />} text="Log in with Facebook" />
+        <LoginButtonComp icon={<EmailSVG />} text="Log in with Email" />
+      </div>
 
-        <p className='login_privacy_txt'>by cliking log in you agree to the <strong>terms of service</strong> and <strong>privacy policy</strong></p>
+      <p className="login_privacy_txt">
+        By clicking Log in, you agree to the <strong>terms of service</strong>{" "}
+        and <strong>privacy policy</strong>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,29 @@
+import { getAuth, signOut } from "firebase/auth";
 import Policy from "../components/Policy";
 import Button from "../components/button/Button";
 import UserAvatarName from "../components/userAvtarName/UserAvatarName";
 import "./Home.css";
+import { app } from "../components/FirebaseConfig";
 
 const Home = () => {
+
+  const handleLogout = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+     
+    } catch (error) {
+      console.error("Logout Error:", error);
+    }
+  };
+
   return (
     <div className="home_page">
       <div className="user_avatar_info">
         <UserAvatarName name="Despo" avatar="" />
       </div>
 
-      <div className="home_page_btns">
+      <div className="home_page_btns" onClick={handleLogout}>
         <Button title="NEW GAME" />
         <Button title="HOW TO PLAY" />
         <Button title="RATINGS" />
