a862a86304f6810e03852582d7edc1dcbf28b83f
Despo <tornike.despotashvili@gmail.com>
2024-03-07 13:02:45 +0400

    create polic component and import to home page

---
+++ src/components/Policy.tsx
@@ -0,0 +1,5 @@
+const Policy = () => {
+  return <p><strong>terms</strong> and <strong>privacy policy</strong></p>
+}
+
+export default Policy
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,26 @@
-import Button from "../components/button/Button"
-import "./Home.css"
+import Policy from "../components/Policy";
+import Button from "../components/button/Button";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home_page">
+      <div className="user_avatar_info">
 
-        <div className="user_avatar_info">
-            
-        </div>
-        
-        <div className="home_page_btns">
-        <Button title="New Game"/>
-        </div>
+      </div>
 
-        <p>privacy</p>
+      <div className="home_page_btns">
+        <Button title="NEW GAME" />
+        <Button title="HOW TO PLAY" />
+        <Button title="RATINGS" />
+        <Button title="LOG OUT" />
+      </div>
+
+      <div className="home_page_policy">
+        <Policy />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
