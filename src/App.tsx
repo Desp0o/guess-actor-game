665f00d60659cab49390cf9437c316f958aaad65
Despo <tornike.despotashvili@gmail.com>
2024-03-07 12:42:34 +0400

    create Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./Login"
 import OutletLayOut from "./components/OutletLayOut"
+import Home from "./pages/Home"
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<OutletLayOut />} />
       <Route index element={<Login />} />
+
+      <Route path="/pages/Home" element={<Home />} />
     </Routes>
     </>
   )
