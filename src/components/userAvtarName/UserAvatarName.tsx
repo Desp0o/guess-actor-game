51913eb87a64ce8856d64e2127fe491d68705e5d
Despo <tornike.despotashvili@gmail.com>
2024-03-13 23:27:02 +0400

    update user avatar on login logout

--- src/components/userAvtarName/UserAvatarName.tsx
+++ src/components/userAvtarName/UserAvatarName.tsx
@@ -1,11 +1,12 @@
 import "./UserAvatarName.css"
+import userDefaultIcon from "../../assets/icons/userDefault.png"
 import {useUser} from "../../hooks/UseUser"
 
 const UserAvatarName = () => {
 const {user, avatar} = useUser()
   return (
     <div className='user_avatar_name'>
-        <img src={avatar} className='user_avatar' alt='user avatar' />
+        <img src={user ? avatar : userDefaultIcon} className='user_avatar' alt='user avatar' />
         <p className="username">{user}</p>
     </div>
   )
